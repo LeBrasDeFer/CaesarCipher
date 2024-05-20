f77a59fab63bf08f98274df1f338174df8c4ca82
LeBrasDeFer <t9776311600@gmail.com>
2024-05-20 20:00:35 +0300

    init commit

---
+++ src/Cipher.java
@@ -0,0 +1,14 @@
+public class Cipher {
+    private char[] alphabet;
+    public Cipher(char[] alphabet) {
+        this.alphabet = alphabet;
+    }
+    public String encrypt(String text, int shift) {
+        // Логика шифрования
+        return null;
+    }
+    public String decrypt(String encryptedText, int shift) {
+        // Логика расшифровки
+        return null;
+    }
+}
